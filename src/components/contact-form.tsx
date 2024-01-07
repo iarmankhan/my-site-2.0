3d41a13081457bbc0e4b03149866367e298a2fc0
Arman Khan <work.armankhan@gmail.com>
2024-01-07 18:05:05 +0530

    Refactor contact form iframe attributes

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -5,8 +5,16 @@ import Script from 'next/script'
 export function ContactForm() {
     return (
         <>
-            <iframe data-tally-src="https://tally.so/embed/npOKz8?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="276" frameborder="0" marginheight="0" marginwidth="0" title="Contact form
-"></iframe>
+            <iframe
+                data-tally-src="https://tally.so/embed/npOKz8?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+                loading="lazy"
+                width="100%"
+                height="276"
+                frameBorder="0"
+                marginHeight={0}
+                marginWidth={0}
+                title="Contact form">
+            </iframe>
             <Script
                 id="tally-js"
                 src="https://tally.so/widgets/embed.js"
